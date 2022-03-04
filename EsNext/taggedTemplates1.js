35b28bcecad8ca21ceefe7130284eacaf1be27db
Ilgner Boer <ilgner.boer@gmail.com>
2022-03-03 21:00:24 -0300

    update class

---
+++ EsNext/taggedTemplates1.js
@@ -0,0 +1,10 @@
+// tagged tempaltes - processa o template dentro de uma funcao
+function tag(partes, ...valores) {
+    console.log(partes)
+    console.log(valores)
+    return 'Outra String'
+}
+
+const aluno = 'Gui'
+const situacao = 'Aprovado'
+console.log(tag `${aluno} esta ${situacao}.`)
